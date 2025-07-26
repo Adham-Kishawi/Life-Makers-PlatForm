123bbc39f8f0e9d32d3876684726138181a684ad
Adham kishawi <adhambinkishawi@gmail.com>
2025-07-27 00:35:41 +0300

    Remove node_modules from Git tracking and add .gitignore

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import Nav from '../Components/Nav';
 import Header from '../Components/Header';
-
-
 import Footer from '../Components/Footer';
+import Banner from './Banner';
 
+import { motion } from 'framer-motion';
+import { FaHandsHelping } from 'react-icons/fa';
+import HomeAboutSection from './HomeAboutSection';
 
 const Home = () => {
   return (
     <>
-       <Header></Header>
-      <Nav/>
+      <Header />
+      <Nav />
+
       <br />
       <br />
       <br />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    
-     <Footer/>
+
+      <motion.div
+        className="text-center py-4"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="font-weight-bold Hellow-Home">
+          <FaHandsHelping className="Hellow-in-Home mr-2" size={28} />
+          <span>أهلا بيكم في موقع </span>
+          <span className="Hellow-in-Home">صناع الحياة الإسماعيلية</span>
+        </h2>
+      </motion.div>
+
+      <Banner />
+
+      <div className="spacer"></div>
+      <HomeAboutSection/>
+       <div className="spacer"></div>
+
+      <Footer />
     </>
   );
 };
